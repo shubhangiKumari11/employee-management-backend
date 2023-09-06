26103ddfa8f9d9ce5da54c97511a0d9dee01cc3e
Shubhangi Kumari <132537962+shubhangiKumari11@users.noreply.github.com>
2023-09-06 19:42:55 +0530

    Add files via upload

---
+++ user.model.js
@@ -0,0 +1,76 @@
+var mongoose=require('mongoose');
+var UserSchema=mongoose.Schema({
+    employeeName:{
+        type:String,
+        required:true
+    },
+    dob:{
+        type:Date,
+        required:true
+    },
+    userName:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    salary:{
+        type:Number,
+        required:false
+    },
+    tasks:{
+        type:[mongoose.Schema.Types.ObjectId],
+        ref:"Task",
+        required:false
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    isCreatedAt:{
+        type:Date,
+        default:new Date()
+    },
+    isDisabled:{
+        type:Boolean,
+        default:false,
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    aadharNumber:{
+        type:String,
+        // here we store a image by using bucket or using multer(store the file at folder and make a id of name of using) 
+        required:true
+    },
+    panCard:{
+        type:String,
+        required:true
+    },
+    photo:{
+        type:String,
+        required:true
+    },
+    projects:{
+        type:Array
+    }
+
+});
+
+module.exports=mongoose.model('User',UserSchema);
